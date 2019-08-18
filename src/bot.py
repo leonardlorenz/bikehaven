34d4eb4cc662ea36146a488c11d86e34041d2ba7
Leonard L <bi0m3tr1xx@gmail.com>
2019-08-19 00:31:02 +0200

    Removed Guild ID

--- src/bot.py
+++ src/bot.py
@@ -3,11 +3,8 @@ from discord.ext import commands
 from discord.utils import get
 from roles import Roles
 
-
-
 PREFIX = '?'
 bot = commands.Bot(command_prefix=PREFIX)
-BIKEHAVEN_ID = 388882163581583360
 
 @bot.command()
 async def ping(ctx):
